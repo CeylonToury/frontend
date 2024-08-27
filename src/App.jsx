d6cfd1b39a734bcc5f67e84d2d418d982ac66310
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-08-27 21:51:13 +0530

    app jsx create routes path

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'antd/dist/reset.css';
 import Home from './pages/Home';
-import SignIn from './pages/signIn';
-import Navbar from './components/layout/Navbar';
+// import NotFound from './pages/NotFound';
+import AppLayout from './components/layout/AppLayout';
+import CustomerRoutes from './routes/CustomerRoutes';
+import HotelRoutes from './routes/HotelRoutes';
+import AdminRoutes from './routes/AdminRoutes';
+// import SignIn from './components/Auth/SignIn';
+import { useAuth } from './components/hooks/useAuth';
+import PrivateRoute from './routes/PrivateRoute';
 
 const App = () => {
-  return (
-    <div>
-      
-      <Routes>
-        {/* <Route path="/" element={<DeliveredMothers />} /> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/navbar" element={<Navbar />} />
+  const { user } = useAuth();
 
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/signin" element={<SignIn />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+
+        <Route element={<PrivateRoute allowedRoles={['ADMIN', 'CUSTOMER', 'GUIDE', 'HOTEL']} />}>
+          <Route path="/" element={<AppLayout />}>
+            <Route path="customer/*" element={<CustomerRoutes />} />
+            <Route path="admin/*" element={<AdminRoutes />} />
+            <Route path="guide/*" element={<GuideRoutes />} />
+            <Route path="hotel/*" element={<HotelRoutes />} />
+          </Route>
+        </Route>
       </Routes>
-     
-    </div>
+    </Router>
   );
 };
 
